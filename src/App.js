3f1bea78490af96577e92a796a638cf8752d947b
Irinel Bandas <ibandas@luc.edu>
2019-09-19 00:00:42 -0500

    updated card format

--- src/App.js
+++ src/App.js
@@ -4,44 +4,56 @@ import { Button, Container, Title, Card, Column, Image, Content, Footer} from 'r
 
 const ProductList = ({products}) => {
   return products.map(product => {
-    return <Column.Group>
-      <Card>
-        <Column key={product.sku}>{product.title}>
-          <Card.Image>
-            <Image.Container size="4by3">
-              {<img src={"data/products/" + product.sku + "_2.jpg"}/>}
-            </Image.Container>
-          </Card.Image>
-          <Card.Content>
-            <Content>
-              {product.description}
-              <Card.Footer>
-                {"S, M, L, XL"}
-              </Card.Footer>
-            </Content>
-          </Card.Content>
-        </Column>
-      </Card>
-    
-    </Column.Group>
-  })
-
-
-  return products.map(product => {
-    return <Column.Group>
-      <Card>
-        <Column key={product.sku}>{product.title}>
-          <Card.Image>
-            {<img src={"data/products/" + product.sku + "_2.jpg"}/>}
-          </Card.Image>
-          <Content>
+    return <Card>
+      <Card.Header>
+        <Card.Header.Title key={product.sku}>{product.title}></Card.Header.Title>
+      </Card.Header>
+      <Card.Content>
+        <Content>
+         <Image.Container size={64}>
+               <Image
+                  src={"data/products/" + product.sku + "_2.jpg"}
+               />
+          </Image.Container>
+        </Content>
+      </Card.Content>
+      <Card.Footer>
+        <Card.Footer.Item as="a" href="#">
           {product.description}
-          </Content>
-          <Card.Footer.Item>{"S, M, L, XL"}</Card.Footer.Item>
-        </Column>
-      </Card>
-    </Column.Group>
+        </Card.Footer.Item>
+        <Card.Footer.Item as="a" href="#">
+          {"Size: S, M, L, XL"}
+        </Card.Footer.Item>
+        <Card.Footer.Item as="a" href="#">
+          {"$" + product.price}
+        </Card.Footer.Item>
+      </Card.Footer>
+    </Card>
   })
+
+
+    // return products.map(product => {
+    //   return <Column.Group>
+    //     <Card>
+    //       <Column key={product.sku}>{product.title}
+    //         <Card.Image>
+    //           <Image.Container size="4by3">
+    //             {<img src={"data/products/" + product.sku + "_2.jpg"}/>}
+    //           </Image.Container>
+    //         </Card.Image>
+    //         <Card.Content>
+    //           <Content>
+    //             {product.description}
+    //             <Card.Footer>
+    //               {"S, M, L, XL"}
+    //             </Card.Footer>
+    //           </Content>
+    //         </Card.Content>
+    //       </Column>
+    //     </Card>
+      
+    //   </Column.Group>
+    // })
 }
 
 const App = () => {
