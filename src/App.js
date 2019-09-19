cb56aefa4132490e6536cc50040fe3fa928771a7
Irinel Bandas <ibandas@luc.edu>
2019-09-18 23:31:44 -0500

    description, sizing, card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import 'rbx/index.css';
+import { Button, Container, Title, Card, Column, Image, Content, Footer} from 'rbx';
+
+const ProductList = ({products}) => {
+  return products.map(product => {
+    return <Column.Group>
+      <Card>
+        <Column key={product.sku}>{product.title}>
+          <Card.Image>
+            <Image.Container size="4by3">
+              {<img src={"data/products/" + product.sku + "_2.jpg"}/>}
+            </Image.Container>
+          </Card.Image>
+          <Card.Content>
+            <Content>
+              {product.description}
+              <Card.Footer>
+                {"S, M, L, XL"}
+              </Card.Footer>
+            </Content>
+          </Card.Content>
+        </Column>
+      </Card>
+    
+    </Column.Group>
+  })
+
+
+  return products.map(product => {
+    return <Column.Group>
+      <Card>
+        <Column key={product.sku}>{product.title}>
+          <Card.Image>
+            {<img src={"data/products/" + product.sku + "_2.jpg"}/>}
+          </Card.Image>
+          <Content>
+          {product.description}
+          </Content>
+          <Card.Footer.Item>{"S, M, L, XL"}</Card.Footer.Item>
+        </Column>
+      </Card>
+    </Column.Group>
+  })
+}
 
 const App = () => {
   const [data, setData] = useState({});
@@ -14,7 +58,7 @@ const App = () => {
 
   return (
     <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
+      <ProductList products={products}/>
     </ul>
   );
 };
