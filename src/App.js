79ababe64cd3a7a3e66d61696f8f50412218361c
Irinel Bandas <ibandas@luc.edu>
2019-12-10 12:47:32 -0600

    product title displayed

--- src/App.js
+++ src/App.js
@@ -1,59 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import 'rbx/index.css';
-import { Button, Container, Title, Card, Column, Image, Content, Footer} from 'rbx';
+// import {Container, Title, Card, Column, Image, Content, Footer} from 'rbx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Card} from 'react-bootstrap';
+
+// const ProductList = ({products}) => {
+//   return products.map(product => {
+//     return <Card>
+//       <Card.Header>
+//         <Card.Header.Title key={product.sku}>{product.title}></Card.Header.Title>
+//       </Card.Header>
+//       <Card.Content>
+//         <Content>
+//          <Image.Container size={64}>
+//                <Image
+//                   src={"data/products/" + product.sku + "_2.jpg"}
+//                />
+//           </Image.Container>
+//         </Content>
+//       </Card.Content>
+//       <Card.Footer>
+//         <Card.Footer.Item as="a" href="#">
+//           {product.description}
+//         </Card.Footer.Item>
+//         <Card.Footer.Item as="a" href="#">
+//           {"Size: S, M, L, XL"}
+//         </Card.Footer.Item>
+//         <Card.Footer.Item as="a" href="#">
+//           {"$" + product.price}
+//         </Card.Footer.Item>
+//       </Card.Footer>
+//     </Card>
+//   })
+// }
+
 
 const ProductList = ({products}) => {
   return products.map(product => {
-    return <Card>
-      <Card.Header>
-        <Card.Header.Title key={product.sku}>{product.title}></Card.Header.Title>
-      </Card.Header>
-      <Card.Content>
-        <Content>
-         <Image.Container size={64}>
-               <Image
-                  src={"data/products/" + product.sku + "_2.jpg"}
-               />
-          </Image.Container>
-        </Content>
-      </Card.Content>
-      <Card.Footer>
-        <Card.Footer.Item as="a" href="#">
+    return <Card bg="dark" text="white" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={"data/products/" + product.sku + "_2.jpg"} />
+      <Card.Body>
+        <Card.Title>key={product.sku}>{product.title}</Card.Title>
+        <Card.Text>
           {product.description}
-        </Card.Footer.Item>
-        <Card.Footer.Item as="a" href="#">
-          {"Size: S, M, L, XL"}
-        </Card.Footer.Item>
-        <Card.Footer.Item as="a" href="#">
-          {"$" + product.price}
-        </Card.Footer.Item>
-      </Card.Footer>
+        </Card.Text>
+        <Button variant="primary">Add to Cart</Button>
+      </Card.Body>
     </Card>
   })
-
-
-    // return products.map(product => {
-    //   return <Column.Group>
-    //     <Card>
-    //       <Column key={product.sku}>{product.title}
-    //         <Card.Image>
-    //           <Image.Container size="4by3">
-    //             {<img src={"data/products/" + product.sku + "_2.jpg"}/>}
-    //           </Image.Container>
-    //         </Card.Image>
-    //         <Card.Content>
-    //           <Content>
-    //             {product.description}
-    //             <Card.Footer>
-    //               {"S, M, L, XL"}
-    //             </Card.Footer>
-    //           </Content>
-    //         </Card.Content>
-    //       </Column>
-    //     </Card>
-      
-    //   </Column.Group>
-    // })
 }
 
 const App = () => {
